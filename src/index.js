6e1dd9b49151df10a9b1f30eae421c94d3f51959
Patrick Allen <allenpatrick86@gmail.com>
2018-02-24 16:29:23 -0800

    Add outline for routes and package.json command functionality

--- src/index.js
+++ src/index.js
@@ -6,3 +6,26 @@ const App = Express()
 const Db = require('./database.js')
 
 const PORT = 3000
+
+//  --- Set JSON & Template Configs ---
+App.use(BodyParser.urlencoded({ extended: true }))
+App.use(BodyParser.json())
+App.use('/static', Express.static(__dirname + '/public'))
+App.set('view engine', 'pug')
+App.set('views', __dirname + '/templates')
+
+// --- Routes
+
+// - Render Landing Page
+
+// - Get User
+
+// - Post User
+
+// - Sys Alive
+
+// --- Setup for `npm run start` command
+
+// - Init Function
+
+// - Start Server
