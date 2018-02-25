d849b05bface5d0ed0a9523208c52c896cec60a9
Patrick Allen <allenpatrick86@gmail.com>
2018-02-24 18:06:45 -0800

    Add broad section headers for framework

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -1,5 +1,29 @@
 // Note: Normally this would be it's own directory for organizational purposes.
 
+// --- Helpers
+var HIDE = 'hidden'
+var BLUR = 'blurred'
+
+// - Generic Helpers
+function removeChildren (el) {
+  while (el.firstChild) { el.removeChild(el.firstChild) }
+}
+
+function toggleClass (el, className) {
+  if (el.classList.contains(className)) {
+    el.classList.remove(className)
+  } else {
+    el.classList.add(className)
+  }
+}
+
+// - Modal Management
+
+// - Error Management
+
+// - User / Form Data: Create & Get
+
+
 window.document.addEventListener('DOMContentLoaded', function () {
 
 })
