4c56d9bac2a13705dd9cf90fee0f5071bbf59a30
Patrick Allen <allenpatrick86@gmail.com>
2018-02-24 16:48:50 -0800

    Add basic validation concept

--- src/database.js
+++ src/database.js
@@ -27,3 +27,43 @@ function isEmptyOrNull (value) {
   // Double Equals used for null/undefined check
   return value == null || value === ''
 }
+
+// --- Validation
+function validateForm (formData) {
+  const { name, username, password } = formData
+  const errorContainer = []
+
+  // - Name
+  if (isEmptyOrNull(name)) {
+    errorContainer.push({ msg: 'Name cannot be empty.', field: 'name' })
+  }
+  // - Username
+  if (isEmptyOrNull(username)) {
+    errorContainer.push({ msg: 'Username cannot be empty.', field: 'username' })
+  }
+  // - Password - Would normally encrypt.
+  if (isEmptyOrNull(password)) {
+    errorContainer.push({ msg: 'Password cannot be empty.', field: 'password' })
+  } else if (password.length < 8) {
+    errorContainer.push({
+      msg: 'Password must be more than 8 characters.',
+      field: 'password',
+    })
+  } else if (!containsUpperCase(password)) {
+    errorContainer.push({
+      msg: 'Password must include capital letters.',
+      field: 'password',
+    })
+  }
+
+  // If we have accumulated Errors, return them:
+  if (errorContainer.length > 0) return errorContainer
+  // Otherwise, return null if no problems:
+  return null
+}
+
+module.exports = {
+  getFormData,
+  updateFormData,
+  validateForm,
+}
